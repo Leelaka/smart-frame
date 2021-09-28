0c0b8c4562f1d8346e187b061b9b5ec100c7d592
Adassuriyage Don Leelaka Indira <liad@deakin.edu.au>
2021-09-28 18:06:49 +1000

    render images from api

--- smart-frame/src/App.js
+++ smart-frame/src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 // import InfoDialog from './InfoDialog';
+import {CircularProgress, LinearProgress} from "@material-ui/core";
 
 function App() {
 
@@ -22,7 +23,7 @@ function App() {
         setProgress((oldProgress) => {
           if(oldProgress >= 100) {
             setIndex((e) => e + 1);
-            return 0
+            return 0;
           }
 
           return oldProgress + 3.34;
@@ -35,10 +36,64 @@ function App() {
     return() => clearInterval(interval);
   }, [index, progress, showInfoDialog]); 
 
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${index}`) //api link
+        .then((res) => {
+            console.log(res.data);
+            setData(res.data);
+            setLoading(false);
+        })
+        .catch((err) => {
+            console.log(err);
+            setLoading(false);
+        });
+  }, [index]);
+
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <>
+    {data ? (
+      <div className ='App'>
+        <div
+          style={{display: 'flex',
+          justifyContent: 'center',
+          
+          height: "100vh" || 1200, //1920 x 1200 static 
+          width: "100vw" || 1920,
+        }}
+        >
+        <img 
+          className="backgroundImg"
+          src={data?.primaryImage}
+          alt="background"
+        />
+
+        <img className='img' src={data?.primaryImage} alt='Primary'/>
+
+        <div className="linearProgressContainer">
+            <LinearProgress variant="determinate" value={progress}/>
+        </div>
+
+      </div> 
+      </div>
+
+    ): (
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+          <CircularProgress size={100} thickness={5} />
+      </div>    
+    )
+    
+    }
+
+    </>
+
   );
 }
 
